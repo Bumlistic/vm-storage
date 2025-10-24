d2e900fdaa605a742751b67c4f8f4432d71701d8
Bumlistic <144963901+Bumlistic@users.noreply.github.com>
2025-10-24 14:54:51 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,3 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "client_id" {
-  description = "Service Principal App (client) ID"
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Service Principal Password / Secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
 variable "location" {
   description = "Azure Region"
   type        = string
