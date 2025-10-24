5a257ef8d9af76089b051d23c692c17f594e4ca3
Bumlistic <144963901+Bumlistic@users.noreply.github.com>
2025-10-24 14:53:24 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
